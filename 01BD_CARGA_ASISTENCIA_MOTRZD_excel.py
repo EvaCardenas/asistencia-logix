69521f1a1c5059ae0016682876ef7cc81e95dcba
EvaCardenas <33786395+EvaCardenas@users.noreply.github.com>
2025-08-03 15:04:56 -0500

    Update 01BD_CARGA_ASISTENCIA_MOTRZD_excel.py

--- 01BD_CARGA_ASISTENCIA_MOTRZD_excel.py
+++ 01BD_CARGA_ASISTENCIA_MOTRZD_excel.py
@@ -32,6 +32,7 @@ print(df_1.head())
 
 # Asegurarse de que la hoja 'REPORTE CITAS' esté presente, eliminando espacios extras
 hoja_nombre = 'REPORTE CITAS'.strip()
+df_2 = archivo_excel_2.parse(hoja_nombre)
 
 # Asegurarse de que la columna 'FECHA_ENTREGA' tiene el formato numérico de Excel
 # Convertir el número de serie de Excel a fecha datetime
